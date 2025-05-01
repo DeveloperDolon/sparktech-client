fc849f31cba0b7adf90373c774a74b263290ce80
DeveloperDolon <dolonr718@gmail.com>
2025-05-01 13:11:13 +0600

    login page designed complete

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -1,9 +1,97 @@
+"use client";
 
+import { Button, Checkbox, Input } from "antd";
+import { Controller, useForm } from "react-hook-form";
+import { GoogleOutlined, LockFilled, MailFilled } from "@ant-design/icons";
+import Link from "next/link";
 
 const LoginForm = () => {
+  const { control, handleSubmit } = useForm();
+
   return (
-    <div>
-      
+    <div className="mt-8">
+      <form className="space-y-6">
+        <div>
+          <label
+            className="md:text-base text-sm font-semibold pb-2 inline-block"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <Input
+                color="#6D42D8"
+                size="large"
+                placeholder="Enter your email"
+                {...field}
+                prefix={
+                  <MailFilled style={{ color: "#6D42D8" }} color="#6D42D8" />
+                }
+              />
+            )}
+          />
+        </div>
+
+        <div>
+          <label
+            className="md:text-base text-sm font-semibold pb-2 inline-block"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <Input
+                color="#6D42D8"
+                size="large"
+                placeholder="Enter your password"
+                {...field}
+                prefix={<LockFilled style={{ color: "#6D42D8" }} />}
+              />
+            )}
+          />
+        </div>
+
+        <div className="flex justify-between items-center">
+          <Checkbox>Remember me</Checkbox>
+
+          <Link href={"/"}>Forget password ?</Link>
+        </div>
+
+        <Button
+          variant="filled"
+          size="large"
+          style={{
+            background: "#6D42D8",
+            color: "white",
+            fontWeight: "bold",
+            borderRadius: "100px",
+          }}
+          className="w-full"
+        >
+          Login
+        </Button>
+        <p className="md:text-base text-sm font-light text-center">
+          Or Sign in With
+        </p>
+        <Button
+          size="large"
+          style={{
+            color: "#6D42D8",
+            fontWeight: "bold",
+            borderRadius: "100px",
+          }}
+          className="w-full"
+          icon={<GoogleOutlined/>}
+        >
+          Google
+        </Button>
+      </form>
     </div>
   );
 };
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,10 +4,10 @@ import loginImage from "@/public/login.png";
 
 const page = () => {
   return (
-    <div className="max-w-7xl bg-red-500 mx-auto flex sm:flex-row flex-col items-center justify-center h-screen">
-      <div><Image src={loginImage} alt="Login image" /></div>
+    <div className="max-w-5xl mx-auto grid md:grid-cols-2 grid-cols-1 items-center h-screen">
+      <div><Image src={loginImage} alt="Login image" className="w-full" /></div>
 
-      <div>
+      <div className="md:px-0 px-6">
         <h1 className="md:text-2xl sm:text-xl text-lg font-semibold">Hello, Welcome</h1>
         <p className="md:text-base text-sm md:mt-5 mt-3.5">Please Enter Your Details Bellow to Continue.</p>
         <LoginForm />
