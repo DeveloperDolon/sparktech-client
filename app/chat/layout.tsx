85436eed30190de3ec3fff4ec87a27226a04e4f6
DeveloperDolon <dolonr718@gmail.com>
2025-06-07 21:56:26 +0600

    route private

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -9,13 +9,14 @@ import {
   CalendarOutlined,
 } from "@ant-design/icons";
 import type { MenuProps } from "antd";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Spin } from "antd";
 import logoImage from "@/public/logo.png";
 import Image from "next/image";
 import "./style.css";
 import { useMeQuery } from "../store/api/auth.api";
+import { useRouter } from "next/navigation";
 
-const {Sider } = Layout;
+const { Sider } = Layout;
 
 type MenuItem = Required<MenuProps>["items"][number];
 
@@ -91,10 +92,37 @@ const ChatLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { data, isLoading } = useMeQuery(1);
+  const route = useRouter();
 
-  const {data, isLoading} = useMeQuery(1);
+  if (isLoading) {
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider width={"120px"} theme="light" />
+        <Layout.Content
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <div>
+            <span>
+              <span>Loading...</span>
+              <br />
+              <br />
+              <Spin size="large" />
+            </span>
+          </div>
+        </Layout.Content>
+      </Layout>
+    );
+  }
 
-  console.log("User Data:", data, isLoading);
+  if (!data?.data?.id) {
+    route.push("/login");
+  }
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -121,12 +149,12 @@ const ChatLayout = ({
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            border: "none"
+            border: "none",
           }}
         />
       </Sider>
 
-      <>{children}</>
+      <>{data?.data?.id ? children : null}</>
     </Layout>
   );
 };
